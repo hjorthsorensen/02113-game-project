247874536643473dac0db77aa4943de31f00d553
Pikachu287 <christianemilmohrbentzen@gmail.com>
2026-06-08 14:25:29 +0200

    kofe

--- firrtl_black_box_resource_files.f
+++ firrtl_black_box_resource_files.f
@@ -1,2 +1,2 @@
-C:\02113 Game\02113-game-project\RamInitSpWf.v
-C:\02113 Game\02113-game-project\RamSpWf.v
+/home/christianbentzen/DTU/Juni 2026 Digital System Design/02113-game-project/RamInitSpWf.v
+/home/christianbentzen/DTU/Juni 2026 Digital System Design/02113-game-project/RamSpWf.v
