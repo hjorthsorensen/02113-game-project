4a22206625bec517c42927f327f9dc96de73fdc3
Noah Christensen <s256075@dtu.dk>
2026-06-08 12:29:57 +0200

    Har lavet min egen gren fuck jer andre i kan sidde på jeres nasty ass grene min gren er røv sej

--- src/main/scala/GameLogicTask1.scala
+++ src/main/scala/GameLogicTask1.scala
@@ -90,6 +90,76 @@ class GameLogicTask1(SpriteNumber: Int, BackTileNumber: Int) extends Module {
   /////////////////////////////////////////////////////////////////
 
 
+val idle :: compute1 :: moveBeer :: done :: Nil = Enum(4)
+  val stateReg = RegInit(idle)
+
+  //Two registers holding the sprite sprite X and Y with the sprite initial position
+  val sprite0XReg = RegInit(32.S(11.W))
+  val sprite0YReg = RegInit((360-32).S(10.W))
+
+  val sprite1XReg = RegInit(128.S(11.W))
+  val sprite1YReg = RegInit((360-32).S(10.W))
+  val sprite1osc = RegInit(0.S(7.W))
+  //A registers holding the sprite horizontal flip
+  val sprite0FlipHorizontalReg = RegInit(false.B)
+  val sprite1FlipHorizontalReg = RegInit(false.B)
+  //Making sprite 0 visible
+  io.spriteVisible(0) := true.B
+  io.spriteVisible(15) := true.B
+  //Connecting resiters to the graphic engine
+  io.spriteXPosition(0) := sprite0XReg
+  io.spriteYPosition(0) := sprite0YReg
+  io.spriteFlipHorizontal(0) := sprite0FlipHorizontalReg
+
+  io.spriteXPosition(15) := sprite1XReg
+  io.spriteYPosition(15) := sprite1YReg
+  io.spriteFlipHorizontal(15) := sprite1FlipHorizontalReg
+
+  //FSMD switch
+  switch(stateReg) {
+    is(idle) {
+      when(io.newFrame) {
+        stateReg := compute1
+      }
+    }
+
+    is(compute1) {
+      when(io.btnD){
+        when(sprite0YReg < (480 - 32 - 24).S) {
+          sprite0YReg := sprite0YReg + 2.S
+        }
+      } .elsewhen(io.btnU){
+        when(sprite0YReg > (96).S) {
+          sprite0YReg := sprite0YReg - 2.S
+        }
+      }
+      when(io.btnR) {
+        when(sprite0XReg < (640 - 32 - 32).S) {
+          sprite0XReg := sprite0XReg + 2.S
+          sprite0FlipHorizontalReg := false.B
+        }
+      } .elsewhen(io.btnL){
+        when(sprite0XReg > 32.S) {
+          sprite0XReg := sprite0XReg - 2.S
+          sprite0FlipHorizontalReg := true.B
+        }
+      }
+      stateReg := moveBeer
+    }
+    is(moveBeer){
+      sprite1osc := sprite1osc + 1.S
+      sprite1XReg := Mux(sprite1osc < 0.S, sprite1XReg + 1.S, sprite1XReg - 1.S)
+      sprite1YReg := Mux(sprite1osc < 0.S, sprite1YReg + 1.S, sprite1YReg - 1.S)
+      sprite1FlipHorizontalReg := Mux(sprite1osc < 0.S, true.B, false.B)
+      stateReg := done
+    }
+    is(done) {
+      io.frameUpdateDone := true.B
+      stateReg := idle
+    }
+  }
+
+
 }
 
 //////////////////////////////////////////////////////////////////////////////
