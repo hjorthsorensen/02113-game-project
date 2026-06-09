c96c4243bc712086e55d052ddf3077b8cbf2d1b7
Noah Christensen <s256075@dtu.dk>
2026-06-09 10:43:41 +0200

    added todo in both spawn and despawn

--- src/main/scala/SpawnCustomer.scala
+++ src/main/scala/SpawnCustomer.scala
@@ -6,11 +6,35 @@ class SpawnCustomer() extends Module {
         val work = Input(Bool())
         val beerDone = Input(Bool())
         val done = Output(Bool())
+        val customer1PosX = Output(SInt(11.W))
+        val customer1PosY = Output(SInt(11.W))
+        val customer2PosX = Output(SInt(11.W))
+        val customer2PosY = Output(SInt(11.W))
     })
 
     //customers default spawn is (0,0); position is picked in spawn state
     val customer1XReg = RegInit(0.S(11.W)) 
     val customer1YReg = RegInit(0.S(10.W)) 
+    val customer1Visible = RegInit(false.B)
+    
+    val customer2XReg = RegInit(0.S(11.W)) 
+    val customer2YReg = RegInit(0.S(10.W)) 
+    val customer2Visible = RegInit(false.B)
+    
+    val customerToSpawn = RegInit(0.U(1.W)) //reg to decide what customer to spawn
+
+
+    //io connections
+    done := false.B
+    io.customer1PosX := customer1XReg
+    io.customer1PosY := customer1YReg
+    io.customer2PosX := customer2XReg
+    io.customer2PosY := customer2YReg
+    
+
+
+
+    //statemachine
     val idle :: spawn :: despawn :: done :: Nil = Enum(4)
     val stateReg = RegInit(idle)
 
@@ -18,10 +42,25 @@ class SpawnCustomer() extends Module {
         is(idle){
             when (io.work){
                 stateReg := spawn
+            }.elsewhen(io.beerDone){
+                stateReg := despawn
             }
         }
         is(spawn){
-            
+            //decide customer
+            //decide location
+            //make visible
+            //set customer pos on output
+        }
+        is(despawn){
+            //decide customer
+            //make visible
+            //change customer decision
+            //
+        }
+        is(done){
+            io.done := true.B
+            stateReg := idle
         }
     }
 
