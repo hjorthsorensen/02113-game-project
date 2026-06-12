15dc4b77f30d348d5ad7ea1296fd3785507dd21f
AugmentedDuck <anders.m.matzen@gmail.com>
2026-06-12 09:28:52 +0200

    Update .gitignore and XDC path in build.tcl
    Add rules to .gitignore to ignore backup and clock info files (*.bak, clockInfo.txt). Update build.tcl to read the project-specific XDC (vivado/Basys3Game/.../GameBasys3.xdc) instead of the generic constraints.xdc, reflecting the relocated/explicit constraints file for the Basys3 project.

--- build.tcl
+++ build.tcl
@@ -4,7 +4,7 @@ set verilog_files [glob -nocomplain *.v]
 
 read_verilog $verilog_files
 
-read_xdc constraints.xdc
+read_xdc vivado/Basys3Game/Basys3Game.srcs/constrs_1/imports/code/GameBasys3.xdc
 
 synth_design -top Top -part xc7a35tcpg236-1
 
