4143637229eb3ae517ddaeb51661418b8af3c344
AugmentedDuck <anders.m.matzen@gmail.com>
2026-06-15 13:37:34 +0200

    Fix

--- src/main/scala/beerLeftFSM.scala
+++ src/main/scala/beerLeftFSM.scala
@@ -4,7 +4,7 @@ import chisel3.util._
 class beerLeftFSM extends Module{
     val io = IO(new Bundle {
         //Inputs
-        val score = Input(UInt(7.W))
+        val score = Input(UInt(8.W))
         val work = Input(Bool())
         
         
@@ -22,18 +22,18 @@ class beerLeftFSM extends Module{
     val stateReg = RegInit(idle)
     // val tile1 :: tile2 :: tile3 :: Nil = Enum(3)
     // val tileReg = RegInit(tile1)
-    val scoreReg = RegInit(0.U(7.W))
+    val scoreReg = RegInit(0.U(8.W))
 
     val RightDigitReg = RegInit(0.U(4.W))
     val MiddleDigitReg = RegInit(0.U(4.W))
     // val wireScore = cat(LeftLeftDigitReg, LeftDigitReg, MiddleDigitReg, RightDigitReg,scoreReg)
 
    def doubleDabble(score: UInt): (UInt, UInt) = {
-    val shiftRegInit = Cat(0.U(8.W), score(15, 0)) // 16bit digits + 16bit input
+    val shiftRegInit = Cat(0.U(8.W), score(7, 0)) // 16bit digits + 16bit input
     var shiftReg = shiftRegInit
     
     for (i <- 0 until 8) {
-      val bcd = shiftReg(25, 16)
+      val bcd = shiftReg(15, 8)
     
       val ones      = bcd(3, 0)
       val tens      = bcd(7, 4)
@@ -43,9 +43,9 @@ class beerLeftFSM extends Module{
     
       val newBcd = Cat(tensAdj, onesAdj)
     
-      shiftReg = Cat(newBcd, shiftReg(15, 0)) << 1
+      shiftReg = Cat(newBcd, shiftReg(7, 0)) << 1
     }
-    val result = shiftReg(25, 16)
+    val result = shiftReg(15, 8)
     (// Return the 4 base10 digits.
       result(3, 0),    // ones
       result(7, 4)    // tens
