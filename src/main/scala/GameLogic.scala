1647f47b23d940e62be32249329cfabcaac9ac8e
AugmentedDuck <anders.m.matzen@gmail.com>
2026-06-15 13:40:26 +0200

    Remove debug LED

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -252,7 +252,6 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
       io.spriteVisible(2) := false.B
       io.spriteVisible(3) := false.B
       io.spriteVisible(11) := false.B
-      io.led(2) := false.B
 
     }
     is(1.U) {
@@ -284,9 +283,6 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
       io.spriteVisible(2)  := false.B
       io.spriteVisible(3)  := false.B 
       io.spriteVisible(11) := false.B
-
-      io.led(2) := true.B
-
     }
   }
 
